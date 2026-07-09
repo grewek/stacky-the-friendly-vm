c9685f229f11f7ba682f3ee25c4e84fc18dd1f17
Kay Grewe <kay.grewe@protonmail.com>
2026-07-09 10:15:05 +0200

    feat: add trim_left and trim_rigth functions to lstrings.h

--- lstring.h
+++ lstring.h
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <ctype.h>
 #include <errno.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -11,6 +12,7 @@ typedef struct {
   size_t length; // the complete length of the string
   const char *data; // pointer that points to the start of the data
 } LString;
+
 
 /*
  * Description: Generate a LString from an exsisting c string
@@ -46,4 +48,67 @@ char *cstring_from_lstring(LString lstring) {
   memcpy(result, lstring.data, lstring.length);
 
   return result;
+}
+
+/*
+ * Description: Trim all whitespace characters from the left side of the string.
+ * @source: The source string that should be trimmed
+ * Returns: The trimmed string
+ * __NOTE__: This Function modifies the position of the data pointer, always make sure that you treat the
+ *           LString more as a view and not as the owner of the actual data.
+ */
+LString lstring_trim_left(LString source) {
+  assert(source.data != NULL);
+
+  size_t start = 0;
+
+  for(size_t i = 0; i < source.length && isspace(source.data[i]); i++) {
+    start = i;
+  }
+
+  if(start < source.length) {
+    start += 1;
+  }
+
+  LString result = {
+    .length = source.length - start,
+    .data = source.data + start,
+  };
+
+  return result;
+}
+/*
+ * Description: Trim all whitespace characters from the right side of the string.
+ * @source: The source string that should be trimmed
+ * Returns: The trimmed string
+ * __NOTE__: This Function modifies the position of the data pointer, always make sure that you treat the
+ *           LString more as a view and not as the owner of the actual data.
+ */
+LString lstring_trim_right(LString source) {
+  assert(source.data != NULL);
+
+  size_t end = source.length - 1;
+  for(size_t i = end; i > 0 && isspace(source.data[i]); i--) {
+    end = i - source.length;
+    assert(end > 0);
+  }
+
+  if(end > 0) {
+    end -= 1;
+  }
+
+  LString result = {
+    .length = source.length - end,
+    .data = source.data,
+  };
+
+  return result;
+}
+
+LString lstring_trim(LString source) {
+  return lstring_trim_left(lstring_trim_right(source));
+}
+
+LString lstring_split_by_delimiter(LString *source, const char delimiter) {
+
 }
